14a35fa9f4742cc02b2524571a844e71d3ccd58c
이규민 <90364652+20210815@users.noreply.github.com>
2024-04-01 17:46:25 +0900

    react 시작

---
+++ demo (1)/demo/src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping
+    public String testController() {
+        return "Hello world!";}
+
+
+    @GetMapping("/testGetMapping")
+    public String testControllerWithPath() {
+        return "Hello World! testGetMapping";
+    }
+
+    @GetMapping("/{id}")
+    public String testControllerWithPathVariable(@PathVariable(required = false) int id) {
+        return "Hello world! id" + id;
+    }
+
+}
